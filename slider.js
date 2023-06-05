aa0b1e22d565bd2e507195c685d26b0f848a1755
zefirez <tsumirez@zoho.com>
2023-06-05 20:20:43 +0200

    improved efficiency of tracking previous image selected mechanic
    Now it relies on 3 variables no matter the amount of images in the slider,
    unlike previous version

--- slider.js
+++ slider.js
@@ -1,13 +1,17 @@
 let slider = document.getElementById("slider");
-let slides = document.querySelectorAll(".slider > input");
+let slides = slider.querySelectorAll(".slider > input");
 let backButton = document.getElementById('back-button');
 let forwardButton = document.getElementById('forward-button');
 let numVisibleSlides = 3;
-let prevSlide = 0;
+let markedInput = slides[0];
+let prevInput = slides[0];
 
 let form = document.getElementsByTagName("form")[0];
 form.addEventListener('change', (e) => {
-    console.log(e.target)
+    markedInput.classList.toggle('previous');
+    prevInput.classList.toggle('previous');
+    markedInput=prevInput;
+    prevInput = e.target;
 })
 
 
@@ -16,14 +20,6 @@ backButton.addEventListener('click', (e) => {
 })
 
 
- slides.forEach((slide, index) => {
-    //  slide.style.transitionDelay = (0.5 * index) + "s";
-    slide.addEventListener('change', (e) => {
-        slider.getElementsByClassName('previous')[0].classList.toggle('previous')
-        slides[prevSlide].classList.toggle('previous');
-        prevSlide = index;
-    })
- })
 
 // let sheet = document.getElementById("gallery-styles").sheet;
 // let lastCssRule = sheet.cssRules.length;
